7617513b0fc62620e799262170127210cd39f559
Irina <irina270511@gmail.com>
2018-06-11 12:38:20 +0300

    edited functions: drawHistogramBar, drawText and renderStatistics

--- js/stat.js
+++ js/stat.js
@@ -9,9 +9,7 @@ var BAR_GAP = 50; // отступ между колонками
 var BAR_WIDTH = 40; // ширина колонки
 var MAX_BAR_HEIGHT = 150; // максимальная высота колонки
 var TEXT_HEIGHT = 16; // высота шрифта
-
-var textFloor = CLOUD_HEIGHT + CLOUD_Y - GAP; // нижний край текста
-var barFloor = CLOUD_HEIGHT + CLOUD_Y - (GAP * 2 + TEXT_HEIGHT); // нижний край колонок
+var RED_COLOR = 'rgba(255, 0, 0, 1)'; // цвет столбика гистограммы для текущего игрока
 
 // отрисовка облака
 var renderCloud = function (ctx, x, y, color) {
@@ -31,6 +29,7 @@ var getMaxElement = function (arr) {
 };
 
 // получение случайного цвета
+// тип аргументов red, green, blue: int
 var getRandomColor = function (red, green, blue) {
   if (red === undefined) {
     red = (Math.round(Math.random() * 255));
@@ -46,38 +45,59 @@ var getRandomColor = function (red, green, blue) {
 };
 
 // отрисовка столбика гистограммы
-var drawHistogramBar = function (ctx, color, index, name, time, maxTime) {
-  var actualHeight = MAX_BAR_HEIGHT * time / maxTime; // высота актуальной колонки
-
-  ctx.fillStyle = color;
-  if (name === 'Вы') {
-    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+// obj = описание столбика гистограммы, тип: объект со свойствами index(int), color(string), name(string), time(int)
+// maxTime = максимальное время среди всех игроков, тип: int
+var drawHistogramBar = function (ctx, obj, maxTime) {
+  ctx.fillStyle = obj.color;
+  if (obj.name === 'Вы') {
+    ctx.fillStyle = RED_COLOR;
   }
-
-  ctx.fillRect(CLOUD_X + BAR_GAP * (index + 1) + BAR_WIDTH * index, barFloor - actualHeight, BAR_WIDTH, actualHeight);
+  var barHeight = MAX_BAR_HEIGHT * obj.time / maxTime; // высота колонки
+  var barCoordinateX = CLOUD_X + BAR_GAP * (obj.index + 1) + BAR_WIDTH * obj.index; // координата по оси Х
+  var barCoordinateY = CLOUD_HEIGHT + CLOUD_Y - (GAP * 2 + TEXT_HEIGHT) - barHeight; // координата по оси Y
+  ctx.fillRect(barCoordinateX, barCoordinateY, BAR_WIDTH, barHeight);
 };
 
-// отрисовка текста (с одинаковой координатой Х)
-var drawText = function (ctx, style, index, text, coordinateY) {
-  ctx.fillStyle = style;
-  ctx.fillText(text, CLOUD_X + BAR_GAP * (index + 1) + BAR_WIDTH * index, coordinateY);
+// отрисовка текста
+// text = текст, тип: string
+// coordinateX = координата по оси Х, тип: int
+// coordinateY = координата по оси Y, тип: int
+var drawText = function (ctx, text, coordinateX, coordinateY) {
+  ctx.font = '16px PT Mono';
+  ctx.fillStyle = '#000';
+  ctx.fillText(text, coordinateX, coordinateY);
 };
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', CLOUD_X + BAR_GAP, CLOUD_Y + GAP + TEXT_HEIGHT);
-  ctx.fillText('Список результатов:', CLOUD_X + BAR_GAP, CLOUD_Y + GAP + TEXT_HEIGHT * 2);
+  var headingCoordinateX = CLOUD_X + BAR_GAP;
+  var firstStringCoordinateY = CLOUD_Y + GAP + TEXT_HEIGHT;
+  var secondStringCoordinateY =  CLOUD_Y + GAP + TEXT_HEIGHT * 2;
+  drawText(ctx, 'Ура вы победили!', headingCoordinateX, firstStringCoordinateY);
+  drawText(ctx, 'Список результатов:', headingCoordinateX, secondStringCoordinateY);
 
   var maxTime = Math.round(getMaxElement(times)); // максимальное время
 
   for (var i = 0; i < names.length; i++) {
-    drawText(ctx, '#000', i, names[i], textFloor);
-    drawHistogramBar(ctx, getRandomColor(0, 0, 255), i, names[i], times[i], maxTime);
-    drawText(ctx, '#000', i, Math.round(times[i]), barFloor - GAP - MAX_BAR_HEIGHT * times[i] / maxTime);
+    var playerName = names[i];
+    var playerNameCoordinateX = CLOUD_X + BAR_GAP * (i + 1) + BAR_WIDTH * i;
+    var playerNameCoordinateY = CLOUD_HEIGHT + CLOUD_Y - GAP;
+    drawText(ctx, playerName, playerNameCoordinateX, playerNameCoordinateY);
+
+    var histogramBar = {
+      index: i,
+      color: getRandomColor(0, 0, 255),
+      name: names[i],
+      time: times[i]
+    };
+    drawHistogramBar(ctx, histogramBar, maxTime);
+
+    var playerTime = Math.round(times[i]);
+    var playerTimeCoordinateX = CLOUD_X + BAR_GAP * (i + 1) + BAR_WIDTH * i;
+    var playerTimeCoordinateY = CLOUD_HEIGHT + CLOUD_Y - (GAP * 2 + TEXT_HEIGHT) - GAP - MAX_BAR_HEIGHT * times[i] / maxTime;
+    drawText(ctx, playerTime, playerTimeCoordinateX, playerTimeCoordinateY);
   }
 
 };
