0440e1df637a152f3084ec283618d72c88c1aa10
Irina <irina270511@gmail.com>
2018-06-17 15:44:55 +0300

    added open&close popup + change colors of wizard_s elements

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,9 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COATS_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
 /**
  * Выдает случайный элемент из массива.
@@ -16,8 +19,6 @@ var randomizeElement = function (arr) {
   var randomElement = arr[index];
   return randomElement;
 };
-
-// СПОСОБ ПЕРВЫЙ, ИСПРАВИЛА, КОПИРУЮ МАССИВ ПЕРЕД УДАЛЕНИЕМ ЭЛ-ТОВ ИЗ НЕГО
 
 /**
  * Выдает случайный элемент из массива, удаляя этот элемент из входящего массива.
@@ -101,12 +102,113 @@ var drawWizard = function (wizards, place) {
   place.appendChild(fragment);
 };
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
 
+// Открытие-закрытие окна настройки персонажа
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = document.querySelector('.setup-close');
+
+/**
+ * Закрывает окно настройки персонажа по нажатию ESC, если фокус не на поле с вводом имени персонажа.
+ *
+ * @param {object} evt - объект event.
+ */
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE && !document.querySelector('input[name=username]:focus')) {
+    setup.classList.add('hidden');
+  }
+};
+
+/**
+ * Открывает окно настройки персонажа.
+ */
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+/**
+ * Закрывает окно настройки персонажа.
+ */
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if(evt.keyCode === ENTER_KEYCODE){
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+// Возможность изменения параметров персонажа по нажатию
+var wizardCoat = document.querySelector('.setup-wizard .wizard-coat');
+var wizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
+var wizardFireball = document.querySelector('.setup-fireball-wrap');
+
+/**
+ * Добавляет или меняет заданное цветовое CSS-свойство стиля на случайный цвет из заданного массива цветов. Также меняет значение инпута для отправки заданного цвета на сервер.
+ *
+ * @param {array} colors - массив возможных цветов.
+ * @param {object} element - DOM-элемент, у которого меняется свойство.
+ * @param {string} property - CSS-свойство, значением которого может быть цвет.
+ * @param {string} inputName - имя инпута, для отправки значения на сервер.
+ */
+var changeColorElement = function (colors, element, property, inputName) {
+  var color = randomizeElement(colors);
+  element.style = property + ': ' + color + ';';
+  document.querySelector('input[name=' + inputName + ']').value = color;
+};
+
+wizardCoat.addEventListener('click', function () {
+  changeColorElement(WIZARD_COATS_COLORS, wizardCoat, 'fill', 'coat-color');
+});
+
+wizardCoat.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    changeColorElement(WIZARD_COATS_COLORS, wizardCoat, 'fill', 'coat-color');
+  }
+});
+
+wizardEyes.addEventListener('click', function () {
+  changeColorElement(WIZARD_EYES_COLORS, wizardEyes, 'fill', 'eyes-color');
+});
+
+wizardEyes.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    changeColorElement(WIZARD_EYES_COLORS, wizardEyes, 'fill', 'eyes-color');
+  }
+});
+
+wizardFireball.addEventListener('click', function () {
+  changeColorElement(WIZARD_FIREBALL_COLORS, wizardFireball, 'background', 'fireball-color');
+});
+
+wizardFireball.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    changeColorElement(WIZARD_FIREBALL_COLORS, wizardFireball, 'background', 'fireball-color');
+  }
+});
+
+// Список подобных персонажей
 var similarListElement = document.querySelector('.setup-similar-list');
 
 var wizards = getWizards(4);
 drawWizard(wizards, similarListElement);
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+setup.querySelector('.setup-similar').classList.remove('hidden');
+
